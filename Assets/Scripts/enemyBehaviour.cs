edebca1ed5f1fc863c883fc8a7e500cc6262c098
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-11 00:38:20 +0100

    start of the work

---
+++ Assets/Scripts/enemyBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myPrefab;
+    void Start()
+    {
+        InvokeRepeating("LaunchProjectile", 2.0f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LaunchProjectile()
+    {
+        Instantiate(myPrefab, transform.position + new Vector3(0.5f, 2, 0), Quaternion.identity);
+
+    }
+}
