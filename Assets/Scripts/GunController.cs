19d43ff7497d11d4998a6ca33aa86bd32ccf9100
Kigaru <jakubster9@gmail.com>
2020-07-11 17:39:18 +0100

    k_zone has proper gun pivots & guns look where the cursor is

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -9,31 +9,52 @@ public class GunController : MonoBehaviour
     GameObject bullet, gunPivot;
     [SerializeField]
     float bulletSpeed=1;
-
+    Vector3 mousePos;
 
     // Update is called once per frame
     void Update()
     {
+        rotateWeapon();
+        checkForFire();
+    }
+
+    void rotateWeapon()
+    {
+        mousePos = new Vector3(Camera.main.ScreenPointToRay(Input.mousePosition).origin.x, Camera.main.ScreenPointToRay(Input.mousePosition).origin.y, 0);
+
+        transform.LookAt(mousePos);
+        if (transform.position.x > mousePos.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.x+ 180);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, transform.rotation.eulerAngles.x+180);
+
+        }
+    }
+
+
+    void checkForFire()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-
-            Vector3 mousePos = new Vector3(Camera.main.ScreenPointToRay(Input.mousePosition).origin.x, Camera.main.ScreenPointToRay(Input.mousePosition).origin.y,0);
-
             GameObject projectile = Instantiate(bullet, gunPivot.transform.position, Quaternion.identity);
             projectile.GetComponent<Bullet>().setSpeed(bulletSpeed);
             projectile.transform.LookAt(mousePos);
             if (transform.position.x > mousePos.x)
             {
-                projectile.transform.rotation = Quaternion.Euler(180, 180, projectile.transform.rotation.eulerAngles.x);
+                projectile.transform.rotation = Quaternion.Euler(0, 0, projectile.transform.rotation.eulerAngles.x+ 180);
             }
             else
             {
-                projectile.transform.rotation = Quaternion.Euler(180, 0, projectile.transform.rotation.eulerAngles.x);
+                projectile.transform.rotation = Quaternion.Euler(0, 180, projectile.transform.rotation.eulerAngles.x+ 180);
 
             }
 
         }
     }
+
 }
 
 
