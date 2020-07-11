2b08512489f5d8d2c50df623ca73f042d997c163
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-11 22:16:04 +0100

    coin

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(50 * Time.deltaTime, 50 * Time.deltaTime, 50 * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
