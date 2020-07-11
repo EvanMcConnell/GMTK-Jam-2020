ed7f06635bdf70eab25ffeb86d92d24019ec602c
Kigaru <jakubster9@gmail.com>
2020-07-11 01:06:13 +0100

    Particles can spawn at mouse point

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject particle;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(particle, Camera.main.ScreenPointToRay(Input.mousePosition).origin, transform.rotation);
+        }
+    }
+}
