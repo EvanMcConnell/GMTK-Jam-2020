b323536d8db1a00fd6a0e0600a28d9dde6c4a9ee
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-11 14:24:18 +0100

    enemey limit

--- Assets/Scripts/enemyBehaviour.cs
+++ Assets/Scripts/enemyBehaviour.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class enemyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject myPrefab;
     public GameObject player;
+    public int maxEnemies = 20;
+    GameObject[] allEnemies;
+    int EnemyAmount;
     public float thrust = 1.0f;
     Rigidbody2D rb;
     Vector2 m_NewPosition;
@@ -23,17 +25,26 @@ public class enemyBehaviour : MonoBehaviour
         //force indirection of player on spawn
         rb.AddForce((player.transform.position - transform.position).normalized * thrust * Time.smoothDeltaTime);
 
+        allEnemies = GameObject.FindGameObjectsWithTag("ballEnemy");
+        EnemyAmount = allEnemies.Length;
+        print("enemyAmount: " + EnemyAmount);
+
         //start spawn
         InvokeRepeating("spawn", 2.0f, 2.0f);
-
-
     }
 
-    // Update is called once per frame
     void spawn()
     {
         rb.AddForce((player.transform.position - transform.position).normalized * thrust * Time.smoothDeltaTime);
-        Instantiate(myPrefab, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+        print("enemyAmount: " + EnemyAmount);
+        print("maxEnemies: " + maxEnemies);
 
+        allEnemies = GameObject.FindGameObjectsWithTag("ballEnemy");
+        EnemyAmount = allEnemies.Length;
+
+        if (EnemyAmount < maxEnemies)
+        {
+            Instantiate(myPrefab, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
+        }
     }
 }
