58a940a6827943fde7af11b27ea0512c3342dd1e
Evan McConnell <mcconnell.evan@gmail.com>
2020-07-11 19:05:37 +0100

    Background Layer Fix

---
+++ Assets/Scripts/buttonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour
+{
+    public menuAnimationController menuA;
+    public menuAnimationController menuB;
+    public menuAnimationController.state menuANewState;
+    public menuAnimationController.state menuBNewState;
+
+    public void transitionMenus()
+    {
+        menuA.setState(menuANewState);
+        menuB.setState(menuBNewState);
+    }
+}
---
+++ Assets/Scripts/menuAnimationController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuAnimationController : MonoBehaviour
+{
+    public enum state { enterLeft, enterRight, exitLeft, exitRight };
+    public state menuState;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (menuState)
+        {
+            case state.enterLeft:
+                anim.SetBool("enterLeft", true);
+                anim.SetBool("enterRight", false);
+                anim.SetBool("exitLeft", false);
+                anim.SetBool("exitRight", false);
+                break;
+
+            case state.enterRight:
+                anim.SetBool("enterLeft", false);
+                anim.SetBool("enterRight", true);
+                anim.SetBool("exitLeft", false);
+                anim.SetBool("exitRight", false);
+                break;
+
+            case state.exitLeft:
+                anim.SetBool("enterLeft", false);
+                anim.SetBool("enterRight", false);
+                anim.SetBool("exitLeft", true);
+                anim.SetBool("exitRight", false);
+                break;
+
+            case state.exitRight:
+                anim.SetBool("enterLeft", false);
+                anim.SetBool("enterRight", false);
+                anim.SetBool("exitLeft", false);
+                anim.SetBool("exitRight", true);
+                break;
+        }
+    }
+
+    public void setState(state newState)
+    {
+        menuState = newState;
+    }
+}
+
+
