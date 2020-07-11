2533836653cc863dd7b63b5a01294aefe4323039
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-11 21:36:11 +0100

    this is a game

--- Assets/Scripts/windowsShatter.cs
+++ Assets/Scripts/windowsShatter.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class windowsShatter : MonoBehaviour
 {
     public GameObject BrokenWindow;
+    public int ShatterPreasure = 2;
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if ((col.gameObject.tag == "player" || col.gameObject.tag == "ballEnemy") && col.relativeVelocity.magnitude > 2)
+        if ((col.gameObject.tag == "player" || col.gameObject.tag == "ballEnemy" || col.gameObject.tag == "Bullet") && col.relativeVelocity.magnitude > ShatterPreasure)
         {
             Instantiate(BrokenWindow, transform.position, transform.rotation);
             Destroy(gameObject);
