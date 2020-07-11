4268fd69f5fec7f61478e1f4aaf9ef7b1102bc84
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-11 13:32:51 +0100

    Enemy balls

--- Assets/Scripts/enemyBehaviour.cs
+++ Assets/Scripts/enemyBehaviour.cs
@@ -6,26 +6,25 @@ public class enemyBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject myPrefab;
-    GameObject player;
+    public GameObject player;
     public float thrust = 1.0f;
-    Rigidbody rb;
+    Rigidbody2D rb;
+    Vector2 m_NewPosition;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("player");
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce((player.transform.position - transform.position).normalized * thrust * Time.smoothDeltaTime);
         InvokeRepeating("spawn", 2.0f, 2.0f);
+        var cubeRenderer = gameObject.GetComponent<Renderer>();
+        cubeRenderer.material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        let dir = player.transform.position - transform.position;
-        dir = dir.normalized;
-        rb.AddForce(dir * thrust);
-    }
-
     void spawn()
     {
-        Instantiate(myPrefab, transform.position + new Vector3(0.5f, 0.5f, 0), Quaternion.identity);
+        rb.AddForce((player.transform.position - transform.position).normalized * thrust * Time.smoothDeltaTime);
+        Instantiate(myPrefab, transform.position + new Vector3(0, 0, 0), Quaternion.identity);
 
     }
 }
