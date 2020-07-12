b924216c491713bfae7cc1d4b19abd3fbd8e2a93
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-12 17:44:19 +0100

    music stop

---
+++ Assets/destroyFunMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyFunMusic : MonoBehaviour
+{
+    void Start()
+    {
+        if (GameObject.Find("gameMusic(Clone)"))
+        {
+            Destroy(GameObject.Find("gameMusic(Clone)"));
+        }
+    }
+
+
+}
