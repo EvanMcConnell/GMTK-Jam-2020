3a5d00854e6cf8f3fa764260ddbfd7a45cbf9646
Ciarán Malone <ciaranmalone27@gmail.com>
2020-07-12 13:51:09 +0100

    new pit level

---
+++ Assets/glassShatterTimeEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glassShatterTimeEvent : MonoBehaviour
+{
+    public GameObject BrokenWindow;
+    public float timeBreak = 15;
+    void Start()
+    {
+        StartCoroutine(shatterGlass());
+    }
+
+
+    IEnumerator shatterGlass()
+    {
+        yield return new WaitForSeconds(timeBreak);
+        Instantiate(BrokenWindow, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
